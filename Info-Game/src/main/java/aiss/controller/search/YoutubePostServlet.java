69d5b1164399832ada15262f38924ce780437bb6
blackylyzard <eemv99@gmail.com>
2019-04-30 18:14:21 +0200

    Post en yt (no probado)

---
+++ Info-Game/src/main/java/aiss/controller/search/YoutubePostServlet.java
@@ -0,0 +1,42 @@
+package aiss.controller.search;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import aiss.model.resources.YoutubePostResource;
+
+public class YoutubePostServlet {
+	
+	private static final Logger log = Logger.getLogger(YoutubePostServlet.class.getName());
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+	String videoId = request.getParameter("videoId");
+	String comment = request.getParameter("comment");
+	String accessTokenYT = (String) request.getSession().getAttribute("Youtube-token");
+			
+	// Add song to playlist
+	YoutubePostResource a = new YoutubePostResource();
+	if(a.createComment(comment, accessTokenYT,videoId)!=null);
+	boolean success=true;
+	
+	if (success) {
+		request.setAttribute("message", "Comment added successfully");
+		log.log(Level.FINE, "Todo bien");
+	}
+	else {
+		request.setAttribute("message", "The comment could not be added");
+		log.log(Level.FINE, "Todo mal");
+	}
+	
+	request.getRequestDispatcher("/success.jsp").forward(request, response);
+
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
