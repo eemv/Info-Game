ae71c1e5eed437eb2353e84803bc89e271120ae5
blackylyzard <47613461+blackylyzard@users.noreply.github.com>
2019-05-28 10:39:40 +0200

    Update YoutubePostResourceTest.java

--- Info-Game/src/test/java/resources/YoutubePostResourceTest.java
+++ Info-Game/src/test/java/resources/YoutubePostResourceTest.java
@@ -13,7 +13,7 @@ public class YoutubePostResourceTest {
 	@Test
 	public void testYoutube() throws UnsupportedEncodingException {
 		
-	String accessToken = "ya29.GlsMB9Ke9wbmdq9DBf0J0k6Wou8et3EdjDCvSY-jT_j0EbiYVSo_EV4hGFkGnJB3blRuo-O4uHogi0BGHVlFTJdvb5IDlqnd0-PJnqN_NfXuqBozA7tlbxKoZ5ZR";
+	String accessToken = "";
 	String query = "Comentario";
 	YoutubeResource ytResource = new YoutubeResource(accessToken);
 	
